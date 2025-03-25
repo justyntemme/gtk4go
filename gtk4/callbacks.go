3907e1b0cbcdcde0b9f8c2dc02b3e5f1df567359
justyn Temme <justyntemme@gmail.com>
2025-03-25 13:59:07 -0500

    1/2 re-implement callback system

---
+++ gtk4/callbacks.go
@@ -0,0 +1,432 @@
+// Package gtk4 provides a unified callback management system for GTK4 widgets
+// File: gtk4go/gtk4/callbacks.go
+package gtk4
+
+// #cgo pkg-config: gtk4
+// #include <gtk/gtk.h>
+// #include <stdlib.h>
+//
+// // Exported callback functions (implemented in Go)
+// extern void callbackHandler(GObject *object, gpointer data);
+// extern void callbackHandlerWithParam(GObject *object, gpointer param, gpointer data);
+// extern gboolean callbackHandlerWithReturn(GObject *object, gpointer data);
+//
+// // Generic function to connect a signal to a handler
+// static gulong connectSignal(GObject *object, const char *signal, gboolean hasParam, gboolean hasReturn, guint callbackId) {
+//     if (hasReturn) {
+//         return g_signal_connect(object, signal, G_CALLBACK(callbackHandlerWithReturn), GUINT_TO_POINTER(callbackId));
+//     } else if (hasParam) {
+//         return g_signal_connect(object, signal, G_CALLBACK(callbackHandlerWithParam), GUINT_TO_POINTER(callbackId));
+//     } else {
+//         return g_signal_connect(object, signal, G_CALLBACK(callbackHandler), GUINT_TO_POINTER(callbackId));
+//     }
+// }
+//
+// // Function to disconnect a signal
+// static void disconnectSignal(GObject *object, gulong handlerId) {
+//     if (handlerId > 0) {
+//         g_signal_handler_disconnect(object, handlerId);
+//     }
+// }
+import "C"
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"unsafe"
+)
+
+// SignalType represents the type of GTK signal
+type SignalType string
+
+// Common GTK signal types
+const (
+	// Button signals
+	SignalClicked SignalType = "clicked"
+	
+	// Entry signals
+	SignalChanged SignalType = "changed"
+	SignalActivate SignalType = "activate"
+	
+	// Window signals
+	SignalCloseRequest SignalType = "close-request"
+	
+	// Dialog signals
+	SignalResponse SignalType = "response"
+	
+	// ListView signals
+	SignalListActivate SignalType = "activate"
+	
+	// SelectionModel signals
+	SignalSelectionChanged SignalType = "selection-changed"
+	
+	// Adjustment signals
+	SignalValueChanged SignalType = "value-changed"
+	
+	// Action signals
+	SignalActionActivate SignalType = "activate"
+)
+
+// nextCallbackID is a counter for generating unique callback IDs
+var nextCallbackID atomic.Uint64
+
+// CallbackManager handles GTK signal callbacks
+type CallbackManager struct {
+	// Map from callback ID to callback data
+	callbacks     sync.Map
+	// Map from object pointer to list of handler IDs
+	objectHandlers sync.Map
+}
+
+// callbackData stores information about a callback
+type callbackData struct {
+	callback    interface{}
+	objectPtr   uintptr
+	signal      SignalType
+	hasParam    bool
+	hasReturn   bool
+	handlerID   C.gulong
+}
+
+// Global callback manager
+var globalCallbackManager = &CallbackManager{}
+
+// Connect connects a signal to a callback function
+func Connect(object interface{}, signal SignalType, callback interface{}) (handlerID uint64) {
+	// Get the object's pointer
+	objectPtr := getObjectPointer(object)
+	if objectPtr == 0 {
+		return 0 // Invalid object
+	}
+
+	// Generate a unique ID for this callback
+	id := nextCallbackID.Add(1)
+
+	// Check callback signature to determine parameter and return type
+	hasParam, hasReturn := analyzeCallbackSignature(callback)
+
+	// Create callback data
+	data := &callbackData{
+		callback:    callback,
+		objectPtr:   objectPtr,
+		signal:      signal,
+		hasParam:    hasParam,
+		hasReturn:   hasReturn,
+		handlerID:   0, // Will be set after connection
+	}
+
+	// Connect the signal
+	cObject := (*C.GObject)(unsafe.Pointer(objectPtr))
+	cSignal := C.CString(string(signal))
+	defer C.free(unsafe.Pointer(cSignal))
+
+	// Connect and get handler ID
+	handlerId := C.connectSignal(
+		cObject,
+		cSignal,
+		boolToGBoolean(hasParam),
+		boolToGBoolean(hasReturn),
+		C.guint(id),
+	)
+	
+	// Store the handler ID in the callback data
+	data.handlerID = handlerId
+
+	// Store the callback data in the map
+	globalCallbackManager.callbacks.Store(id, data)
+
+	// Associate this handler with the object for cleanup
+	globalCallbackManager.trackObjectHandler(objectPtr, handlerId)
+
+	return id
+}
+
+// Disconnect disconnects a signal handler by its ID
+func Disconnect(id uint64) {
+	// Look up the callback data
+	value, ok := globalCallbackManager.callbacks.Load(id)
+	if !ok {
+		return
+	}
+	
+	data := value.(*callbackData)
+	
+	// Disconnect the signal
+	cObject := (*C.GObject)(unsafe.Pointer(data.objectPtr))
+	C.disconnectSignal(cObject, data.handlerID)
+	
+	// Remove the callback from the map
+	globalCallbackManager.callbacks.Delete(id)
+	
+	// Remove the handler from the object's handler list
+	globalCallbackManager.untrackObjectHandler(data.objectPtr, data.handlerID)
+}
+
+// DisconnectAll disconnects all signal handlers for an object
+func DisconnectAll(object interface{}) {
+	objectPtr := getObjectPointer(object)
+	if objectPtr == 0 {
+		return
+	}
+	
+	// Get the object's handlers
+	value, ok := globalCallbackManager.objectHandlers.Load(objectPtr)
+	if !ok {
+		return
+	}
+	
+	handlers := value.([]C.gulong)
+	
+	// Disconnect each handler
+	cObject := (*C.GObject)(unsafe.Pointer(objectPtr))
+	for _, handlerId := range handlers {
+		C.disconnectSignal(cObject, handlerId)
+	}
+	
+	// Remove the object from the map
+	globalCallbackManager.objectHandlers.Delete(objectPtr)
+	
+	// Remove all callbacks for this object from the callbacks map
+	globalCallbackManager.callbacks.Range(func(key, value interface{}) bool {
+		data := value.(*callbackData)
+		if data.objectPtr == objectPtr {
+			globalCallbackManager.callbacks.Delete(key)
+		}
+		return true
+	})
+}
+
+// getObjectPointer returns the pointer to the GObject of a GTK widget
+func getObjectPointer(object interface{}) uintptr {
+	// Handle common GTK widget types
+	switch obj := object.(type) {
+	case Widget:
+		return uintptr(unsafe.Pointer(obj.GetWidget()))
+	case *Adjustment:
+		return uintptr(unsafe.Pointer(obj.adjustment))
+	case *Action:
+		return uintptr(unsafe.Pointer(obj.action))
+	default:
+		// Try to find a GetWidget or Native method using reflection
+		val := reflect.ValueOf(object)
+		if val.Kind() == reflect.Ptr && !val.IsNil() {
+			// Try GetWidget method
+			getWidgetMethod := val.MethodByName("GetWidget")
+			if getWidgetMethod.IsValid() {
+				results := getWidgetMethod.Call(nil)
+				if len(results) == 1 {
+					return uintptr(unsafe.Pointer(results[0].Pointer()))
+				}
+			}
+			
+			// Try Native method
+			nativeMethod := val.MethodByName("Native")
+			if nativeMethod.IsValid() {
+				results := nativeMethod.Call(nil)
+				if len(results) == 1 && results[0].Kind() == reflect.Uintptr {
+					return results[0].Uint()
+				}
+			}
+		}
+	}
+	
+	return 0 // Unable to get pointer
+}
+
+// analyzeCallbackSignature determines if a callback takes parameters or returns a value
+func analyzeCallbackSignature(callback interface{}) (hasParam bool, hasReturn bool) {
+	// Get the type of the callback
+	callbackType := reflect.TypeOf(callback)
+	
+	// Must be a function
+	if callbackType.Kind() != reflect.Func {
+		return false, false
+	}
+	
+	// Check if it has parameters
+	hasParam = callbackType.NumIn() > 0
+	
+	// Check if it has a return value
+	hasReturn = callbackType.NumOut() > 0
+	
+	return hasParam, hasReturn
+}
+
+// trackObjectHandler associates a handler ID with an object
+func (m *CallbackManager) trackObjectHandler(objectPtr uintptr, handlerId C.gulong) {
+	value, ok := m.objectHandlers.Load(objectPtr)
+	var handlers []C.gulong
+	if ok {
+		handlers = value.([]C.gulong)
+	} else {
+		handlers = make([]C.gulong, 0, 4) // Pre-allocate space for 4 handlers
+	}
+	
+	handlers = append(handlers, handlerId)
+	m.objectHandlers.Store(objectPtr, handlers)
+}
+
+// untrackObjectHandler removes a handler ID from an object
+func (m *CallbackManager) untrackObjectHandler(objectPtr uintptr, handlerId C.gulong) {
+	value, ok := m.objectHandlers.Load(objectPtr)
+	if !ok {
+		return
+	}
+	
+	handlers := value.([]C.gulong)
+	
+	// Find and remove the handler
+	for i, id := range handlers {
+		if id == handlerId {
+			// Remove by swapping with the last element and slicing
+			handlers[i] = handlers[len(handlers)-1]
+			handlers = handlers[:len(handlers)-1]
+			break
+		}
+	}
+	
+	if len(handlers) == 0 {
+		// No more handlers for this object
+		m.objectHandlers.Delete(objectPtr)
+	} else {
+		m.objectHandlers.Store(objectPtr, handlers)
+	}
+}
+
+// boolToGBoolean converts a Go bool to a C gboolean
+func boolToGBoolean(b bool) C.gboolean {
+	if b {
+		return C.TRUE
+	}
+	return C.FALSE
+}
+
+// Exported callback functions for CGo
+
+//export callbackHandler
+func callbackHandler(object *C.GObject, data C.gpointer) {
+	id := uint64(uintptr(data))
+	value, ok := globalCallbackManager.callbacks.Load(id)
+	if !ok {
+		return
+	}
+	
+	callbackData := value.(*callbackData)
+	
+	// Call the callback with no parameters
+	if callback, ok := callbackData.callback.(func()); ok {
+		callback()
+	}
+}
+
+//export callbackHandlerWithParam
+func callbackHandlerWithParam(object *C.GObject, param C.gpointer, data C.gpointer) {
+	id := uint64(uintptr(data))
+	value, ok := globalCallbackManager.callbacks.Load(id)
+	if !ok {
+		return
+	}
+	
+	callbackData := value.(*callbackData)
+	
+	// Handle different callback signatures based on the signal type
+	switch callbackData.signal {
+	case SignalResponse:
+		// For dialog responses, param is the response ID
+		if callback, ok := callbackData.callback.(func(ResponseType)); ok {
+			callback(ResponseType(uintptr(param)))
+		}
+	case SignalListActivate:
+		// For list view activation, param is the position
+		if callback, ok := callbackData.callback.(func(int)); ok {
+			callback(int(uintptr(param)))
+		}
+	case SignalSelectionChanged:
+		// For selection changed, we have position and count
+		// Note: This is a simplification as we're only passing position
+		if callback, ok := callbackData.callback.(func(int)); ok {
+			callback(int(uintptr(param)))
+		} else if callback, ok := callbackData.callback.(func(int, int)); ok {
+			// In a real implementation, you'd extract both position and count
+			callback(int(uintptr(param)), 0)
+		}
+	default:
+		// For other cases, try to call with an int parameter
+		if callback, ok := callbackData.callback.(func(int)); ok {
+			callback(int(uintptr(param)))
+		}
+	}
+}
+
+//export callbackHandlerWithReturn
+func callbackHandlerWithReturn(object *C.GObject, data C.gpointer) C.gboolean {
+	id := uint64(uintptr(data))
+	value, ok := globalCallbackManager.callbacks.Load(id)
+	if !ok {
+		return C.FALSE
+	}
+	
+	callbackData := value.(*callbackData)
+	
+	// Call the callback with no parameters and get boolean return
+	if callback, ok := callbackData.callback.(func() bool); ok {
+		result := callback()
+		if result {
+			return C.TRUE
+		}
+	}
+	
+	return C.FALSE
+}
+
+// Initialize the callback system
+func init() {
+	// Register a finalizer to clean up all callbacks at exit
+	runtime.SetFinalizer(globalCallbackManager, func(m *CallbackManager) {
+		// Disconnect all signals
+		m.callbacks.Range(func(key, value interface{}) bool {
+			data := value.(*callbackData)
+			cObject := (*C.GObject)(unsafe.Pointer(data.objectPtr))
+			C.disconnectSignal(cObject, data.handlerID)
+			return true
+		})
+	})
+}
+
+// Helper: GetStats returns statistics about the callback system
+func GetCallbackStats() map[string]int {
+	stats := make(map[string]int)
+	
+	// Count callbacks
+	callbackCount := 0
+	globalCallbackManager.callbacks.Range(func(_, _ interface{}) bool {
+		callbackCount++
+		return true
+	})
+	stats["TotalCallbacks"] = callbackCount
+	
+	// Count objects with handlers
+	objectCount := 0
+	globalCallbackManager.objectHandlers.Range(func(_, _ interface{}) bool {
+		objectCount++
+		return true
+	})
+	stats["ObjectsWithCallbacks"] = objectCount
+	
+	// Count callback types by signal
+	signalCounts := make(map[SignalType]int)
+	globalCallbackManager.callbacks.Range(func(_, value interface{}) bool {
+		data := value.(*callbackData)
+		signalCounts[data.signal]++
+		return true
+	})
+	
+	for signal, count := range signalCounts {
+		stats[fmt.Sprintf("Signal_%s", signal)] = count
+	}
+	
+	return stats
+}
